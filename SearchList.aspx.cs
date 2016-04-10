80ccf4b03a99b25fa46726e431fdcb3d9e59f1d7
jianrongzheng <U1573041@unimail.hud.ac.uk>
2016-04-10 14:15:54 +0100

    Connect to database show the results on datalist

---
+++ SearchList.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+public partial class SearchList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            gvbind();
+        }
+    }
+    private void gvbind()
+    {
+        string key = "";
+        string sql = "";
+        if (Request.QueryString["StartT"] != null)
+        {
+            key = Request.QueryString["StartT"].ToString();
+            sql = "select * from TInfo where    StartT like '%" + key + "%' ";
+        }
+        if (Request.QueryString["ET"] != null)
+        {
+            key = Request.QueryString["ET"].ToString();
+            sql = "select * from TInfo where   ET like '%" + key + "%'  ";
+        }
+        if (Request.QueryString["STime"] != null)
+        {
+            key = Request.QueryString["STime"].ToString();
+            sql = "select * from TInfo where  STime like '%" + key + "%' ";
+        }
+
+        SqlDataReader dr = Maticsoft.DBUtility.DbHelperSQL.ExecuteReader(sql);
+        this.DataList1.DataSource = dr;
+        this.DataList1.DataBind();
+
+    }
+    protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
