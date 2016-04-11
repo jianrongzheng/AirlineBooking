03f226437a1554c69309e03404d4ab8cb29f8bfc
jianrongzheng <U1573041@unimail.hud.ac.uk>
2016-04-11 14:29:24 +0100

    Function of Administrator Log out

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -8,23 +8,17 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
-using System.Data.SqlClient;
-public partial class MasterPage : System.Web.UI.MasterPage
+
+public partial class Admin_MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (!IsPostBack)
-        {
-            Get();
-        }
-
+        Label1.Text = Session["Usernasme"].ToString();
     }
-    private void Get()
+ 
+    protected void LKBtcxt_Click(object sender, EventArgs e)
     {
-
+        Response.Redirect("../Login.aspx");
     }
-    protected void lbtnALogin_Click(object sender, EventArgs e)
-    {
-        Response.Write("<script language=javascript>window.open('AdminManage/Login.aspx','','width=455,height=255')</script>");
-    }
+   
 }
